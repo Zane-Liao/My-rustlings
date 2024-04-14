f376cd9936956ed2d8e3a1ad687f593053f651b8
Zane_Liao <lzq666amn@gmail.com>
2024-04-14 23:34:39 +0800

    4/14

---
+++ exercises/intro/intro2.rs
@@ -0,0 +1,10 @@
+// intro2.rs
+// Make the code print a greeting to the world.
+// Execute `rustlings hint intro2` or use the `hint` watch subcommand for a hint.
+
+// I AM NOT DONE
+
+fn main() {
+    let s: &str = "world";
+    println!("Hello {}!", s);
+}
