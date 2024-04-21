effbfb05b6d8f2869ab7ae20491233ffe05d85c3
Zane_Liao <lzq666amn@gmail.com>
2024-04-22 00:33:55 +0800

    2024/4/22

--- exercises/move_semantics/move_semantics5.rs
+++ exercises/move_semantics/move_semantics5.rs
@@ -7,9 +7,11 @@
 
 fn main() {
     let mut x = 100;
-    let y = &mut x;
+    {
+        let y = &mut x;
+        *y += 100;
+    }
     let z = &mut x;
-    *y += 100;
     *z += 1000;
     assert_eq!(x, 1200);
 }
--- exercises/move_semantics/move_semantics6.rs
+++ exercises/move_semantics/move_semantics6.rs
@@ -7,19 +7,17 @@
 fn main() {
     let data = "Rust is great!".to_string();
 
-    get_char(data);
+    get_char(&data);
 
     string_uppercase(&data);
 }
 
 // Should not take ownership
-fn get_char(data: String) -> char {
+fn get_char(data: &String) -> char {
     data.chars().last().unwrap()
 }
 
 // Should take ownership
 fn string_uppercase(mut data: &String) {
-    data = &data.to_uppercase();
-
-    println!("{}", data);
+    println!("{}", data.to_uppercase());
 }
