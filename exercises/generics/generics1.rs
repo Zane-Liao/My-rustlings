708d14ee5b06e760333f11a7bab17bc755ba0c98
Zane_Liao <lzq666amn@gmail.com>
2024-04-26 21:36:20 +0800

    4/26

--- exercises/generics/generics1.rs
+++ exercises/generics/generics1.rs
@@ -6,6 +6,6 @@
 // I AM NOT DONE
 
 fn main() {
-    let mut shopping_list: Vec<?> = Vec::new();
-    shopping_list.push("milk");
+    let mut shopping_list: Vec<String> = Vec::new();
+    shopping_list.push("milk".to_string());
 }
